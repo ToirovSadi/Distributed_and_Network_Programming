8f015309f30d7887d697bf222d4432f067c1546d
Toirov Sadi <sadi.toirov2003@gmail.com>
2023-03-30 22:00:29 +0300

    feat: serve multiple users simultaneously

--- lab1/SadiToirov.py
+++ lab1/SadiToirov.py
@@ -2,7 +2,7 @@ import argparse
 import os
 import socket
 
-MSS = 20476  # MSS = Server buffer size (20480) - data header size (4)
+MSS = 20480  # MSS = Server buffer size (20480) - data header size (4)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("server_port", type=str)
@@ -13,41 +13,56 @@ server_ip = '127.0.0.1'
 udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 udp_socket.bind((server_ip, server_port))
 
+class File:
+    def __init__(self, file_io=None, size=None, name=None, seqno=1):
+        self.file_io = file_io
+        self.size = size
+        self.name = name
+        self.seqno = seqno
+
+files = dict()
 print(f'({server_ip}, {server_port}): Listening...')
 
-# receive start packet from client
-data, addr = udp_socket.recvfrom(MSS)
-data = data.decode().split('|')
-if data[0] != 's':
-    print('Server: expected start comand "s", got:', data[0])
-    exit(1)
+try:
+    while True:
+        data, addr = udp_socket.recvfrom(MSS)
+        typ = data[0:1].decode()
+        if typ == 's':
+            data_decoded = data.decode().split('|')
+            print(addr, data.decode())
+            seqno = (int(data_decoded[1]) + 1) % 2
+            file_name = data_decoded[2]
+            file_size = int(data_decoded[3])
+            packet = f'a|{seqno}'
+            udp_socket.sendto(packet.encode(), addr)
+            if os.path.exists(file_name):
+                print(f"{(server_ip, server_port)}: rewriting the file: {file_name}")
+            files[addr] = File(
+                file_io=open(file_name, "bw"),
+                name=file_name,
+                size=file_size,
+            )
+        elif typ == 'd':
+            if(addr not in files):
+                print(f'{(server_ip, server_port)}: error got unregistred address')
+                continue
+            print(addr, data)
+            seqno = (int(data[2:3].decode()) + 1) % 2
+            if files[addr].seqno != seqno:
+                files[addr].seqno = (files[addr].seqno + 1) % 2
+                files[addr].file_io.write(data[4:])
+                files[addr].size -= len(data[4:])
+                if files[addr].size <= 0:
+                    print(f'{(server_ip, server_port)}: Received {files[addr].name}.')
+                    files[addr].file_io.close()
+            packet = f'a|{seqno}'
+            udp_socket.sendto(packet.encode(), addr)
+        else:
+            print(f'{(server_ip, server_port)}: unknown command, got: {typ}')
+            break
+except KeyboardInterrupt:
+    print(f'{(server_ip, server_port)}: Shutting down...')
+    udp_socket.close()
+    exit(0)
 
-print(addr, '|'.join(data))
-seqno = (int(data[1]) + 1) % 2
-file_name = data[2]
-file_size = int(data[3])
-packet = f'a|{seqno}'
-udp_socket.sendto(packet.encode(), addr)
-
-if not os.path.exists(file_name):
-    print(f"{(server_ip, server_port)}: rewriting the file: {file_name}")
-
-# open file a start receiving data and write it to the file
-file = open(file_name, "bw")
-while True:
-    data, addr = udp_socket.recvfrom(MSS)
-    if data[0:1].decode() != 'd':
-        print('Server: expected data comand "d", got:', data[0:1].decode())
-        exit(1)
-    print(addr, data)
-    seqno = (int(data[2:3].decode()) + 1) % 2
-    file.write(data[4:])
-    packet = f'a|{seqno}'
-    udp_socket.sendto(packet.encode(), addr)
-    file_size -= len(data[4:])
-    
-    if file_size <= 0:
-        print(f'{(server_ip, server_port)}: Received {file_name}.')
-        break
-
-file.close()
+udp_socket.close()
